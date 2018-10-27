26d18bddb5f9c6b001cd8b85caefa44e5e16889e
Kleber Silva <kb-max@hotmail.com>
2018-10-27 15:25:14 -0300

    FuncionarioService interface added

---
+++ src/main/java/com/myproject/pontointeligente/api/service/FuncionarioService.java
@@ -0,0 +1,40 @@
+package com.myproject.pontointeligente.api.service;
+
+import java.util.Optional;
+
+import com.myproject.pontointeligente.api.entities.Funcionario;
+
+public interface FuncionarioService {
+
+	/**
+	 * Persiste um funcionario no banco de dados
+	 * 
+	 * @param funcionario
+	 * @return Funcionario
+	 */
+	Funcionario persistir(Funcionario funcionario);
+
+	/**
+	 * Busca e retorna um funcionario pelo cpf
+	 * 
+	 * @param cpf
+	 * @return Optional<Funcionario>
+	 */
+	Optional<Funcionario> buscarPorCnpj(String cpf);
+
+	/**
+	 * Busca e retorna um funcionario pelo email
+	 * 
+	 * @param email
+	 * @return
+	 */
+	Optional<Funcionario> buscarPorEmail(String email);
+
+	/**
+	 * Busca e retorna um funcionario pelo id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	Optional<Funcionario> buscarPorId(Long id);
+}
